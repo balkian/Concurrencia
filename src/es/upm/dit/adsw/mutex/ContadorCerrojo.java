dbc66dc59b3c63abfac839f5cd69ecd1bf485577
Juan A. de la Puente <jpuente@dit.upm.es>
2020-03-23 13:06:11 +0100

    add ContadorCerrojo

--- src/es/upm/dit/adsw/mutex/ContadorCerrojo.java
+++ src/es/upm/dit/adsw/mutex/ContadorCerrojo.java
@@ -8,7 +8,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * Es seguro en un entorno concurrente
 
  * @author jpuente
- * @version 2019.03.06
+ * @version 2020.03.23
  */
 public class ContadorCerrojo {
 
@@ -19,7 +19,7 @@ public class ContadorCerrojo {
 		cuenta = valorInicial;
 	}
 
-	public void incrementar() {
+	public void incrementa() {
 		cerrojo.lock();
 		cuenta++;
 		cerrojo.unlock();
@@ -27,5 +27,48 @@ public class ContadorCerrojo {
 
 	public long valor() {
 		return cuenta;
+	}
+	
+	/**
+	 * Smoke test
+	 */
+	public static void main(String[] args) {
+
+		final long nVeces = 1000000;
+		final int nThreads = 2;
+
+		ContadorCerrojo contador = new ContadorCerrojo(0);
+
+		/**
+		 * Hebra que incrementa el contador nVeces
+		 */
+		class Cuenta extends Thread {
+			@Override
+			public void run() {
+				for (long i = 0; i < nVeces; i++) {
+					contador.incrementa();
+				}
+			}
+		}
+
+		Cuenta[] hebra = new Cuenta[nThreads];
+
+		System.out.println(nThreads + " contadores incrementando " + "la cuenta " + nVeces + " veces cada uno");
+
+		for (int id = 0; id < nThreads; id++) {
+			hebra[id] = new Cuenta();
+			hebra[id].start();
+		}
+
+		for (int id = 0; id < nThreads; id++) {
+			try {
+				hebra[id].join();
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+
+		System.out.print("cuenta = " + contador.valor());
+		System.out.println("; debería ser " + nThreads * nVeces);
 	}
 }
